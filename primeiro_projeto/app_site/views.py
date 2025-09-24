2aa6895d1c740ff50ad5c0fea002bc0eb19ad787
Dennis Rocha <dennis0201@gmail.com>
2025-09-23 21:48:34 -0300

    Terceira aula commit 1 -Desenvolvimento de sistemas

--- primeiro_projeto/app_site/views.py
+++ primeiro_projeto/app_site/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'app_site/pages/index.html')
+
+def sobre(request):
+    return render(request, 'app_site/pages/sobre.html')
--- primeiro_projeto/primeiro_projeto/urls.py
+++ primeiro_projeto/primeiro_projeto/urls.py
@@ -20,5 +20,6 @@ from app_site import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('sobre/', views.sobre),
 ]
