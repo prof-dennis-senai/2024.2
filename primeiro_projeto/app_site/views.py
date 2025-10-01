480fe1b4e770452b8c9a0008068618fcde328dbe
Dennis Rocha <dennis0201@gmail.com>
2025-09-30 22:41:40 -0300

    aula

--- primeiro_projeto/app_site/views.py
+++ primeiro_projeto/app_site/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'app_site/pages/index.html')
+    return render(request, 'app_site/pages/index.html', {"nome_teste": "Guilherme"})
 
 def sobre(request):
-    return render(request, 'app_site/pages/sobre.html')
+    return render(request, 'app_site/pages/sobre.html', {"nome_teste": "Joao"})
