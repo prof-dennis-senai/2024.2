35efa740f1e50365c7f499bf55658009400cafe7
Dennis Rocha <dennis0201@gmail.com>
2025-11-04 22:03:45 -0300

    commit 3 aula 8

--- projeto_controle/app_gestao/admin.py
+++ projeto_controle/app_gestao/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Gestao
 
 # Register your models here.
-admin.site.register(Gestao)
+@admin.register(Gestao)
+class GestaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf_formatado', 'cargo', 'eh_terceiro', 'ativo')
+    search_fields = ('nome', 'cpf', 'cargo')
+    list_filter = ('eh_terceiro', 'ativo')
--- projeto_controle/app_treinamento/admin.py
+++ projeto_controle/app_treinamento/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Treinamento, TreinamentoColaborador
 
 # Register your models here.
-admin.site.register(Treinamento)
-admin.site.register(TreinamentoColaborador)
+@admin.register(Treinamento)
+class TreinamentoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'validade_em_meses', 'quantidade_horas_formatada')
+
+
+
+@admin.register(TreinamentoColaborador)
+class TreinamentoColaboradorAdmin(admin.ModelAdmin):
+    list_display = ('treinamento', 'colaborador', 'data_treinamento', 'dias_restantes_do_treinamento', 'treinamento_esta_em_dia')
+    search_fields = ('treinamento', 'colaborador')  
