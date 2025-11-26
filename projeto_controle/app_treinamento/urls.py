2c02dbe1d925cf57c5ad5c87611b9e155fb69998
Dennis Rocha <dennis0201@gmail.com>
2025-11-25 21:44:29 -0300

    commit 1 aula 12

--- projeto_controle/app_treinamento/urls.py
+++ projeto_controle/app_treinamento/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('cadastrar/', views.cadastrar_treinamento, name='cadastrar_treinamento'),
     path('editar/<int:id>', views.editar_treinamento, name='editar_treinamento'),
     path('remover/<int:id>', views.remover_treinamento, name='remover_treinamento'),
+    path('cursos/', views.listar_cursos, name='listar_cursos'),
+    path('cursos/cadastrar/', views.cadastrar_treinamento, name='cadastrar_curso'),
 ]
